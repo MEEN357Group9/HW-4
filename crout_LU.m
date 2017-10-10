b7ce9c2ae1d771ec31e890b0e1c761abac324144
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-10-09 22:43:01 -0500

    much change
    very sum
    wow

--- crout_LU.m
+++ crout_LU.m
@@ -12,16 +12,20 @@ TermU = zeros(1);
 
 for k = 1:n
     for i = k:n
+        summation = 0;
         for m = 1:k-1
             TermL(m) = L(i,m)*U(m,k);
+            summation = summation + TermL(m);
         end
-        L(i,k) = A(i,k) - sum(TermL);
+        L(i,k) = A(i,k) - summation;
     end
     for j = k+1:n
+        summation2 = 0;
         for m = 1:k-1
             TermU(m) = L(k,m)*U(m,j);
+            summation = summation + TermU(m);
         end
-        U(k,j) = (A(k,j) - sum(TermU))/L(k,k);
+        U(k,j) = (A(k,j) - summation2)/L(k,k);
     end
 end
 
