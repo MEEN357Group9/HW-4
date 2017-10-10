95aacc464d3072fd403f1f60cc75bbfb2e02d7f3
HAshby <harleyashby@comcast.net>
2017-10-09 22:42:31 -0500

    Task 2 done

--- TestGDI.m
+++ TestGDI.m
@@ -1,14 +1,26 @@
 %Preparing to sub into backward elim
+clear ALL
+close ALL
+clc
 A=[2,-3,1;5,1,6;4,3,-5];
 y=randi(10,3);
 %code starts here
 [ L,U] = crout_LU( A );
-x=zeros(n^2);
 [n]=size(A);
 n=n(1);
+x=zeros(n,1);
+summation=0;
 for i=n:-1:1
     j=i+1;
+    if j>n;
+        x(i)=y(i)/U(i,i);
+    else
+        for j=1:i-1
     TermsU(i)=(U(i,j)*x(j));
-    
-    
+    summation=summation+TermsU(i);
+        end
+    x(i)=(y(i)-sum(U(i,j)*x(j)))/U(i,i);
+    end
 end
+%code ends here.
+disp(x);
--- backward_elim.m
+++ backward_elim.m
@@ -1,6 +1,22 @@
 function [ x ] = backward_elim( U,y )
 %Backwards sub
-
+[ L,U] = crout_LU( A );
+[n]=size(A);
+n=n(1);
+x=zeros(n,1);
+summation=0;
+for i=n:-1:1
+    j=i+1;
+    if j>n;
+        x(i)=y(i)/U(i,i);
+    else
+        for j=1:i-1
+    TermsU(i)=(U(i,j)*x(j));
+    summation=summation+TermsU(i);
+        end
+    x(i)=(y(i)-sum(U(i,j)*x(j)))/U(i,i);
+    end
+end
 
 end
 
