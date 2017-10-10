428bb5a05da9862b92f69c5b161a6e8e2d0b5a1c
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-10-09 21:43:28 -0500

    wtf
    here we go

---
+++ crout_LU.m
@@ -0,0 +1,27 @@
+function [ L,U] = crout_LU( A )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+L = zeros(n);
+U = eye(n);
+
+TermL = zeros(1);
+TermU = zeros(1);
+
+for k = 1:n
+    for i = k:n
+        for m = 1:k-1
+            TermL(m) = L(i,m)*U(m,k);
+        end
+        L(i,k) = A(i,k) - sum(TermL);
+    end
+    for j = k+1:n
+        for m = 1:k-1
+            TermU(m) = L(k,m)*U(m,j);
+        end
+        U(k,j) = (A(k,j) - sum(TermU))/L(k,k);
+    end
+end
+
+end
+
