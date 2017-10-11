4efdc46bd7d626fe12569d7e3fbce23824cc992a
HAshby <harleyashby@comcast.net>
2017-10-11 14:43:32 -0500

    this should finish my part

--- TestGDI.m
+++ TestGDI.m
@@ -4,25 +4,11 @@ clear ALL
 close ALL
 clc
 A=[2,-3,1;5,1,6;4,3,-5];
-y=randi(10,3);
-%code starts here
+b=[0;1;0];
 [ L,U] = crout_LU( A );
-[n]=size(A);
-n=n(1);
-x=zeros(n,1);
-summation=0;
-for i=n:-1:1
-    j=i+1;
-    if j>n;
-        x(i)=y(i)/U(i,i);
-    else
-        for j=1:i-1
-    TermsU(i)=(U(i,j)*x(j));
-    summation=summation+TermsU(i);
-        end
-    x(i)=(y(i)-sum(U(i,j)*x(j)))/U(i,i);
-    end
-end
-%code ends here.
+[y ] = forward_elim( L,b );
+%code starts here
+[ x ] = backward_elim( U,y )
+%code below here
 X=table(x,'Rownames',{'X1' 'X2' 'X3'});
 disp(X);
--- backward_elim.m
+++ backward_elim.m
@@ -1,7 +1,17 @@
 function [ x ] = backward_elim( U,y )
-%Backwards sub
-[ L,U] = crout_LU( A );
-[n]=size(A);
+%Backwards sub3
+[n]=size(U);
+[N]=size(y);
+if n(1)~=n(2);
+    error('The U matrix is not a square matrix.')
+elseif n(1)~=N(1)
+    error('Matrix y does not have enought columns.')
+end
+for I=1:n
+    if U(I,I)==0
+        error('There is a zero in the dagonial of the U matrix.')
+    end
+end
 n=n(1);
 x=zeros(n,1);
 summation=0;
