5d6deb39fdd113d6895b7a2c9478b1c63231cbb1
HAshby <harleyashby@comcast.net>
2017-10-11 15:58:28 -0500

    created 3b

--- TestGDI.m
+++ TestGDI.m
@@ -5,10 +5,9 @@ close ALL
 clc
 A=[2,-3,1;5,1,6;4,3,-5];
 b=[0;1;0];
-[ L,U] = crout_LU( A );
-[y ] = forward_elim( L,b );
+
 %code starts here
-[ x ] = backward_elim( U,y )
+[x,L,U] = solve_LU( A,b );
 %code below here
 X=table(x,'Rownames',{'X1' 'X2' 'X3'});
 disp(X);
