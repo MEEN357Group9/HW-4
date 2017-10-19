f5edd63d5f75ecb13c333fe93973ccc8c8fc1e5e
HAshby <harleyashby@comcast.net>
2017-10-18 21:59:35 -0500

    awsertyu

--- testp6.m
+++ testp6.m
@@ -1,17 +1,29 @@
-A = [2, -3, 1; 5, 1, 6; 4, 3, -5];
-b = [1, 0, 0]'; %solves for the first column of the invers matrix
-
-[x1, L, U] = solve_LU(A,b);
-
-A_inv = zeros(size(A));
-A_inv(:,1) = x1;
-I = eye(size(A));
-
-
-for i = 2:3
-    y = forward_elim(L,I(:,i));
-    x = backward_elim(U,y);
-    A_inv(:,i) = x;
+clc;
+clear;
+close;
+xdata=1:1:10;
+ydata=1:1:10;
+x=5;
+%code starts here.
+LX=length(xdata);
+LY=length(ydata);
+n=LX;
+y=zeros(1,LX);
+if LX~=LY
+    error('xdata and ydata need to be the same length!')
 end
-
-A_inv*A
+addition=0;
+for j=1:n
+    summation = 0;
+    for i=1:n
+        if i~=j
+            y(i)=((x-xdata(i))/(xdata(j)-xdata(i)))*ydata(j);
+            %needs to fix this
+%             summation=summation+y(i);
+%         else
+%         end
+%     end
+%     addition=addition+summation;
+% end
+% addition 
+            
